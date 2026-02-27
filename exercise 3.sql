ca4f2e9a06d540ff8e8f79d9e59bd11d6e45afbc
komalagrawalcs28-design <komal.agrawal.cs28@iilm.edu>
2026-02-27 13:08:58 +0530

    Add files via upload

---
+++ exercise 3.sql
@@ -0,0 +1,13 @@
+--experiment 3 - tasks
+
+--Find all the Toy Story movies
+SELECT * FROM movies;
+--Find all the movies directed by John Lasseter
+SELECT title, director FROM movies 
+WHERE title LIKE "Toy Story%";
+--Find all the movies (and director) not directed by John Lasseter
+SELECT title, director FROM movies 
+WHERE director = "John Lasseter";
+--Find all the WALL-* movies
+SELECT title, director FROM movies 
+WHERE director != "John Lasseter";
