57ef3611c2f32560a1ee1203df3f3238117f07f5
komalagrawalcs28-design <komal.agrawal.cs28@iilm.edu>
2026-02-27 13:02:56 +0530

    Add files via upload

---
+++ exercise 2.sql
@@ -0,0 +1,13 @@
+-- exercise 2 -tasks
+
+--Find the movie with a row id of 6
+SELECT * FROM movies;
+--Find the movies released in the years between 2000 and 2010
+SELECT id, title FROM movies 
+WHERE id = 6;
+--Find the movies not released in the years between 2000 and 2010
+SELECT title, year FROM movies
+WHERE year BETWEEN 2000 AND 2010;
+--Find the first 5 Pixar movies and their release year
+SELECT title, year FROM movies
+WHERE year <= 2003;
