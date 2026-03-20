8bc07b8ff509b9fdfd294d7ad11f69f675debf02
komalagrawalcs28-design <komal.agrawal.cs28@iilm.edu>
2026-03-20 12:36:41 +0530

    exercise 2.sql

--- exercise 2.sql
+++ exercise 2.sql
@@ -1,13 +1,14 @@
 -- exercise 2 -tasks
 
 --Find the movie with a row id of 6
-SELECT * FROM movies;
---Find the movies released in the years between 2000 and 2010
 SELECT id, title FROM movies 
 WHERE id = 6;
---Find the movies not released in the years between 2000 and 2010
+--Find the movies released in the years between 2000 and 2010
 SELECT title, year FROM movies
 WHERE year BETWEEN 2000 AND 2010;
+--Find the movies not released in the years between 2000 and 2010
+SELECT title, year FROM movies
+WHERE year < 2000 OR year > 2010;
 --Find the first 5 Pixar movies and their release year
 SELECT title, year FROM movies
 WHERE year <= 2003;
