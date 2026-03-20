b034dbdd170d753a5f26eb16280bc5ed412beaf4
komalagrawalcs28-design <komal.agrawal.cs28@iilm.edu>
2026-03-20 13:01:05 +0530

    exercise 10.sql

---
+++ exercise 10.sql
@@ -0,0 +1,12 @@
+-- exercise 10 
+-- 1. Find the longest time that an employee has been at the studio
+SELECT MAX(years_employed) as Max_years_employed
+FROM employees;
+-- 2. For each role, find the average number of years employed by employees in that role
+SELECT role, AVG(years_employed) as Average_years_employed
+FROM employees
+GROUP BY role;
+-- 3. Find the total number of employee years worked in each building
+SELECT building, SUM(years_employed) as Total_years_employed
+FROM employees
+GROUP BY building;
