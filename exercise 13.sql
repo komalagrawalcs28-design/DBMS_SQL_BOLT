34e45f585dc073e3b96e9f10c735d8b3e93bcad9
komalagrawalcs28-design <komal.agrawal.cs28@iilm.edu>
2026-03-23 01:27:26 +0530

    exercise 13.sql

---
+++ exercise 13.sql
@@ -0,0 +1,5 @@
+-- exercise 13 
+-- Add the studio's new production, Toy Story 4 to the list of movies
+INSERT INTO movies VALUES (4, "Toy Story 4", "El Directore", 2015, 90);
+-- Toy Story 4 has been released to critical acclaim! It had a rating of 8.7, and made 340 million domestically and 270 million internationally. Add the record to the BoxOffice table.
+INSERT INTO boxoffice VALUES (4, 8.7, 340000000, 270000000);
