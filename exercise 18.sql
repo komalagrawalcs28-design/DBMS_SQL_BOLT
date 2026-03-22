22a391af638f18d201717d50a275c64ce86891bb
komalagrawalcs28-design <komal.agrawal.cs28@iilm.edu>
2026-03-23 01:38:04 +0530

    exercise 18.sql

---
+++ exercise 18.sql
@@ -0,0 +1,6 @@
+-- exercsie 18
+-- 1. We've sadly reached the end of our lessons, lets clean up by removing the Movies table
+SELECT * FROM movies;
+DROP TABLE Movies;
+-- 2. And drop the BoxOffice table as well
+DROP TABLE BoxOffice;
